ac408006f057507a79775c625eb0cba88ef6724e
rob2fresh4this <grijrob07@gmail.com>
2024-10-16 14:00:17 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+
+var sheep = new bool[] { true, true, false, true, false, false, };
+
+
+int count = 0;
+
+for (int i = 0; i < sheep.Length; i++)
+{
+    if (sheep[i] == true)
+    {
+        count++;
+        Console.WriteLine(count + " sheep are here");
+    }
+}
